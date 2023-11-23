0991404fb4d9e3f8277b204d1526cadf883deda3
Mund3k <74401169+kubat1611@users.noreply.github.com>
2023-11-23 16:09:09 +0100

    added GET and POST

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,7 @@ users = [
     {"id": 1, "name": "Wojciech", "lastname": "Oczkowski"},
     {"id": 2, "name": "Mateusz", "lastname": "Kozlowski"},
 ]
+
 
 
 @app.route('/')
@@ -16,6 +17,32 @@ def start():
 @app.route('/users', methods=['GET'])
 def get_users():
     return jsonify(users), 200
+
+
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user_of_id(id):
+    if id not in [user['id'] for user in users]:
+        return jsonify({"message": "User not found"}), 404
+    user = [user for user in users if user['id'] == id]
+    return jsonify(user), 200
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    if not data or 'name' not in data or 'lastname' not in data:
+        return jsonify({"error": "Invalid request format"}), 400
+
+    new_user = {
+        "id": len(users) + 1,
+        "name": data['name'],
+        "lastname": data['lastname']
+    }
+
+    users.append(new_user)
+
+    return jsonify(new_user), 201
 
 
 if __name__ == '__main__':
