cdcf3d75d20d831bba2f62d66b216be0f63b36a1
Mund3k <74401169+kubat1611@users.noreply.github.com>
2023-11-29 13:27:54 +0100

    poprawione unit testy

--- src/tests/test_unit.py
+++ src/tests/test_unit.py
@@ -1,94 +1,77 @@
 import unittest
 import json
-from src.api.main import app, users
+from src.api.main import app
+from src.api.main import start, get_users, get_user, create_user, update_user, delete_user, users, get_user_by_id
 
 
 class TestAppUnit(unittest.TestCase):
 
-    def setUp(self):
-        self.app = app.test_client()
+    def test_start(self):
+        response = start()
+        self.assertEqual(response, 'Welcome to my API!')
 
-    def test_start_endpoint(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Welcome to my API!')
+    def test_get_users(self):
+        with app.app_context():
+            response, status_code = get_users()
+            self.assertEqual(status_code, 200)
+            data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(len(data), 3)
 
-    def test_get_users_endpoint(self):
-        response = self.app.get('/users')
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, users)
+    def test_create_user(self):
+        with app.app_context():
+            user_data = {'name': 'John', 'lastname': 'Doe'}
 
-    def test_get_single_user_endpoint(self):
-        user_id = 1
-        response = self.app.get(f'/users/{user_id}')
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, users[user_id - 1])
+            with app.test_request_context(method='POST', data=json.dumps(user_data), content_type='application/json'):
+                response, status_code = create_user()
 
-    def test_get_single_user_endpoint_not_found(self):
-        user_id = 999
-        response = self.app.get(f'/users/{user_id}')
-        self.assertEqual(response.status_code, 404)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, {'error': 'User not found'})
+                self.assertEqual(status_code, 201)
+                data = json.loads(response.get_data(as_text=True))
+                self.assertIn('id', data)
 
-    def test_create_user_endpoint(self):
-        new_user_data = {"name": "Jakub", "lastname": "Teterycz"}
-        response = self.app.post('/users', json=new_user_data)
-        self.assertEqual(response.status_code, 201)
-        user_id = json.loads(response.data.decode('utf-8'))['id']
-        created_user = {"id": user_id, **new_user_data}
-        self.assertIn(created_user, users)
+                self.assertEqual(len(users), 3)
+                new_user = users[-1]
+                self.assertEqual(new_user['name'], 'John')
+                self.assertEqual(new_user['lastname'], 'Doe')
 
-    def test_create_user_endpoint_missing_data(self):
-        new_user_data = {"name": "Jakub"}
-        response = self.app.post('/users', json=new_user_data)
-        self.assertEqual(response.status_code, 400)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, {'error': 'Name and lastname are required'})
+    def test_get_user(self):
+        with app.app_context():
+            existing_user_id = 1
+            existing_user_data = {'name': 'John', 'lastname': 'Doe'}
+            users.append({'id': existing_user_id, **existing_user_data})
 
-    def test_update_user_endpoint(self):
-        user_id = 1
-        update_data = {"name": "UpdatedName"}
-        response = self.app.patch(f'/users/{user_id}', json=update_data)
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(users[user_id - 1]['name'], update_data['name'])
+            response, status_code = get_user(existing_user_id)
+            self.assertEqual(status_code, 200)
+            data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(data['id'], existing_user_id)
 
-    def test_update_user_endpoint_not_found(self):
-        user_id = 999
-        update_data = {"name": "UpdatedName"}
-        response = self.app.patch(f'/users/{user_id}', json=update_data)
-        self.assertEqual(response.status_code, 404)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, {'error': 'User not found'})
+            non_existing_user_id = 999
+            response, status_code = get_user(non_existing_user_id)
+            self.assertEqual(status_code, 404)
+            error_data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(error_data['error'], 'User not found')
 
-    def test_update_user_endpoint_invalid_data(self):
-        user_id = 1
-        update_data = {"invalid": "data"}
-        response = self.app.patch(f'/users/{user_id}', json=update_data)
-        self.assertEqual(response.status_code, 400)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, {'error': 'Invalid request body'})
+    def test_update_user(self):
+        with app.app_context():
+            existing_user_id = 1
+            existing_user_data = {'name': 'John', 'lastname': 'Doe'}
+            users.append({'id': existing_user_id, **existing_user_data})
 
-    def test_delete_user_endpoint(self):
-        new_user_data = {"name": "ToDelete", "lastname": "User"}
-        response = self.app.post('/users', json=new_user_data)
-        self.assertEqual(response.status_code, 201)
-        user_id = json.loads(response.data.decode('utf-8'))['id']
-        created_user = {"id": user_id, **new_user_data}
-        self.assertIn(created_user, users)
+            updated_data = {'name': 'UpdatedName'}
 
-        response = self.app.delete(f'/users/{user_id}')
-        self.assertEqual(response.status_code, 204)
-        self.assertNotIn(created_user, users)
+            with app.test_request_context(method='PUT', data=json.dumps(updated_data), content_type='application/json'):
+                response, status_code = update_user(existing_user_id)
 
-    def test_delete_user_endpoint_not_found(self):
-        user_id = 999
-        response = self.app.delete(f'/users/{user_id}')
-        self.assertEqual(response.status_code, 404)
-        data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data, {'error': 'User not found'})
+                self.assertEqual(status_code, 204)
+
+                updated_user = get_user_by_id(existing_user_id)
+                self.assertEqual(updated_user['name'], 'UpdatedName')
+                self.assertEqual(updated_user['lastname'], 'Doe')
+
+    def test_delete_user(self):
+        with app.app_context():
+            user_id = 1
+            response, status_code = delete_user(user_id)
+            self.assertEqual(status_code, 204)
 
 
 if __name__ == '__main__':
